2352c62fc0dfc3bd49aef8a0c367733fc3295c99
milkwitch <dandelionwalk@gmail.com>
2019-02-26 16:20:42 -0500

    added cart page for user

---
+++ client/components/cart.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+
+export class Cart extends Component {
+  render() {
+    return <h1> This is your cart </h1>
+  }
+}
