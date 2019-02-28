6f6bdef49bc776a3936669885cff0eeeb68c33a3
Alex <Jillbk22@gmail.com>
2019-02-27 19:41:29 -0500

    changed css file name

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import '../../public/style-splash.css'
+import '../../public/styleSplash.css'
 
 /**
  * COMPONENT
