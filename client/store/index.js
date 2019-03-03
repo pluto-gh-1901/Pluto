15d97ee918764c57d4c1a91c04704425be05a8cd
Evgeniya Naumova <jainnaumova@gmail.com>
2019-03-03 14:10:00 -0500

    fixed logger for production

--- client/store/index.js
+++ client/store/index.js
@@ -7,12 +7,36 @@ import product from './product'
 import cart from './cart'
 
 const reducer = combineReducers({user, product, cart})
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
+const middleware =
+  process.env.NODE_ENV !== 'production'
+    ? composeWithDevTools(
+        applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+      )
+    : composeWithDevTools(applyMiddleware(thunkMiddleware))
+// applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+
 const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
 export * from './product'
 export * from './cart'
+
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from '../reducers/root_reducer';
+//
+// const middleware = [];
+//
+// middleware.push(thunk);
+//
+// if (process.env.NODE_ENV !== 'production') {
+//   const logger = require('redux-logger');
+//   middleware.push(logger());
+// }
+//
+// const configureStore = (preloadedState = {}) => {
+//   return createStore(rootReducer, preloadedState, applyMiddleware(...middleware));
+// }
+//
+// export default configureStore;
